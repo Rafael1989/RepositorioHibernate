9a9ab9e17f0e494d339335723182c8f64c8921bd
Rafael <Rafael@192.168.0.11>
2017-07-15 14:52:23 -0300

    pag 178

---
+++ hibernate/src/br/com/caelum/hibernate/AdicionaProduto.java
@@ -0,0 +1,28 @@
+package br.com.caelum.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+public class AdicionaProduto {
+	public static void main(String[] args) {
+		Produto p = new Produto();
+		p.setNome("Rafael");
+		p.setDescricao("Hoje é um novo dia");
+		p.setPreco(100.50);
+		
+		AnnotationConfiguration cfg = new AnnotationConfiguration();
+		cfg.addAnnotatedClass(Produto.class);
+		
+		SessionFactory factory = cfg.buildSessionFactory();
+		
+		Session session = factory.openSession();
+		
+		session.beginTransaction();
+		session.save(p);
+		session.getTransaction().commit();
+		
+		System.out.println("Id do produto: " + p.getId());
+		session.close();
+	}
+}
